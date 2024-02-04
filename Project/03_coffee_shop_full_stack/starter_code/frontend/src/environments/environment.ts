e3d3c3e665eeabf876c299f11f10178e61339e70
bon10102 <bon10102@users.noreply.github.com>
2024-02-04 18:29:26 -0500

    Get frontend to build and update environment.ts

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-8rtvxlnr3qtw0oqi.us', // the auth0 domain prefix
+    audience: 'Coffee_Shop', // the audience set for the auth0 app
+    clientId: 'LLxbauzzi8pgoD2DIww7UKtTzPYFg5V4', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
